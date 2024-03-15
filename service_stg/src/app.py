3a178d818f01704f2e557a3559c28fff9014951f
NosenkoAA <NosenkoAA@>
2024-03-15 12:42:38 +0300

    1st

--- service_stg/src/app.py
+++ service_stg/src/app.py
@@ -4,10 +4,10 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask
 
 from app_config import AppConfig
-from sample_app.sample_job import SampleMessageProcessor
+from stg_loader.stg_message_processor_job import StgMessageProcessor
+from stg_loader.repository.stg_repository import StgRepository
 
 app = Flask(__name__)
-
 
 # Заводим endpoint для проверки, поднялся ли сервис.
 # Обратиться к нему можно будет GET-запросом по адресу localhost:5000/health.
@@ -15,7 +15,6 @@ app = Flask(__name__)
 @app.get('/health')
 def health():
     return 'healthy'
-
 
 if __name__ == '__main__':
     # Устанавливаем уровень логгирования в Debug, чтобы иметь возможность просматривать отладочные логи.
@@ -26,10 +25,17 @@ if __name__ == '__main__':
 
     # Инициализируем процессор сообщений.
     # Пока он пустой. Нужен для того, чтобы потом в нем писать логику обработки сообщений из Kafka.
-    proc = SampleMessageProcessor(app.logger)
+    proc = StgMessageProcessor(
+        config.kafka_consumer(),
+        config.kafka_producer(),
+        config.redis_client(),
+        StgRepository(config.pg_warehouse_db()),
+        app.logger,
+        100
+    )
 
     # Запускаем процессор в бэкграунде.
-    # BackgroundScheduler будет по расписанию вызывать функцию run нашего обработчика(SampleMessageProcessor).
+    # BackgroundScheduler будет по расписанию вызывать функцию run нашего обработчика(StgMessageProcessor).
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=proc.run, trigger="interval", seconds=config.DEFAULT_JOB_INTERVAL)
     scheduler.start()
---
+++ service_stg/src/stg_loader/stg_message_processor_job.py
@@ -0,0 +1,73 @@
+from datetime import datetime
+from logging import Logger
+
+from lib.kafka_connect.kafka_connectors import KafkaConsumer, KafkaProducer
+from lib.redis import RedisClient
+from stg_loader.repository.stg_repository import StgRepository
+
+
+class StgMessageProcessor:
+    def __init__(self,
+                 KafkaConsumer: KafkaConsumer,
+                 KafkaProducer: KafkaProducer,
+                 RedisClient: RedisClient,
+                 StgRepository: StgRepository,
+                 logger: Logger,
+                 batch_size: int) -> None:
+        self._consumer = KafkaConsumer
+        self._producer = KafkaProducer
+        self._redis = RedisClient
+        self._stg_repository = StgRepository
+        self._logger = logger
+        self._batch_size = batch_size
+
+    # функция, которая будет вызываться по расписанию.
+    def run(self) -> None:
+        # Пишем в лог, что джоб был запущен.
+        self._logger.info(f"{datetime.utcnow()}: START")
+
+        # Имитация работы. Здесь будет реализована обработка сообщений.
+        for _ in range(self._batch_size):
+            input_msg = self._consumer.consume()
+            if input_msg is None:
+                break
+
+            if input_msg.get('object_id'):
+                self._stg_repository.order_events_insert(
+                    input_msg['object_id'],
+                    input_msg['object_type'],
+                    input_msg['sent_dttm'],
+                    input_msg['payload'],
+                )
+
+                user_id = input_msg['payload']['user']['id']
+                restaurant_id = input_msg['payload']['restaurant']['id']
+
+                user_data = self._redis.get(user_id)
+                restaurant_data = self._redis.get(restaurant_id)
+
+                exit_msg = dict(
+                    object_id=input_msg['object_id'],
+                    object_type=input_msg['object_type'],
+                    payload={
+                        "id": input_msg['object_id'],
+                        "date": input_msg['payload']['date'],
+                        "cost": input_msg['payload']['cost'],
+                        "payment": input_msg['payload']['payment'],
+                        "status": input_msg['payload']['final_status'],
+                        "restaurant": {
+                            "id": restaurant_data['_id'],
+                            "name": restaurant_data['name']
+                        },
+                        "user": {
+                            "id": user_data['_id'],
+                            "name": user_data['name']
+                        },
+                        "products": restaurant_data['menu']
+                    }
+                )
+
+                self._producer.produce(exit_msg)
+
+        # Пишем в лог, что джоб успешно завершен.
+        self._logger.info(f"{datetime.utcnow()}: FINISH")
